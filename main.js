82d4616ccf77a8141dc3c3afc71ff4de38adce2e
Alejandra Alvarez <alejaserna.20@gmail.com>
2023-01-15 23:38:35 -0500

    Se creo la lista de productos desde js

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const menuBurgerIcon = document.querySelector('.menu');
-const menuCarritoIcon = document.querySelector('.navbar-shopping-cart ');
-const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const menuEmail = document.querySelector('.navbar-email')
+const desktopMenu = document.querySelector('.desktop-menu')
+const menuBurgerIcon = document.querySelector('.menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart ')
+const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
-menuBurgerIcon.addEventListener('click', toggleMobileMenu);
-menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+menuEmail.addEventListener('click', toggleDesktopMenu)
+menuBurgerIcon.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
     const isAsideClosed = aside.classList.contains('inactive');
@@ -45,3 +46,76 @@ function toggleCarritoAside() {
 
     aside.classList.toggle('inactive')
 }
+
+const productList = [];
+productList.push({
+    name: 'Chess',
+    price: 60,
+    image: 'https://images.pexels.com/photos/65169/pexels-photo-65169.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Laptop',
+    price: 630,
+    image: 'https://images.pexels.com/photos/7552366/pexels-photo-7552366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Bike',
+    price: 125,
+    image: 'https://images.pexels.com/photos/7499746/pexels-photo-7499746.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+});
+productList.push({
+    name: 'Keyboard',
+    price: 115,
+    image: 'https://images.pexels.com/photos/917463/pexels-photo-917463.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Clock',
+    price: 30,
+    image: 'https://images.pexels.com/photos/714701/pexels-photo-714701.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Desk',
+    price: 210,
+    image: 'https://images.pexels.com/photos/939331/pexels-photo-939331.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+function renderProducts(arr) {
+    for (product of arr){
+        const productCard = document.createElement('div')
+        productCard.classList.add('product-card')
+
+        const ProductImg = document.createElement('img')
+        ProductImg.setAttribute('src', product.image)
+
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+
+        const productInfoDiv = document.createElement('div')
+
+        const productPrice = document.createElement('p')
+        productPrice.innerText = '$' + product.price
+        const productName = document.createElement('p')
+        productName.innerText = product.name
+
+        productInfoDiv.append(productPrice, productName)
+
+        const productInfoFigure = document.createElement('figure')
+        const productImgCart = document.createElement('img')
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+
+        productInfoFigure.appendChild(productImgCart)
+
+        productInfo.append(productInfoDiv, productInfoFigure)
+
+        productCard.append(ProductImg, productInfo)
+
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList)
